cd3ff4ee475a306742ffcc3e44a33977c60ac4f1
Zach Shuffield <zshuffield@forbes.com>
2020-02-26 15:16:51 -0500

    console log style tags after each test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+function countStyleTags(): void {
+  const head: HTMLHeadElement = document.getElementsByTagName('head')[0];
+  const styles: HTMLCollectionOf<HTMLStyleElement> | [] = head.getElementsByTagName('style');
+  let styleTagCount = 0;
+
+  for (let i: number = 0; i < styles.length; i++) {
+    styleTagCount++;
+  }
+  console.log(styleTagCount);
+}
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -9,6 +20,10 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
+  afterEach(() => {
+    countStyleTags();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -28,4 +43,16 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('style-tag-cleanup app is running!');
   });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'style-tag-cleanup'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('style-tag-cleanup');
+  });
+
 });
